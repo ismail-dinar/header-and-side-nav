305fa13f04e46692510f767cb565e043222ca960
ismail-dinar <sm41l.4796@gmail.com>
2022-05-16 22:20:53 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,12 @@ import { Table } from './interfaces/table.interface';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   public table: Table;
 
-  public onTableChnage(table: Table): void {
-    this.table = table
+  public onTableChange(table: Table): void {
+    this.table = table;
   }
 }
-
-
