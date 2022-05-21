3c80a43c799b6ba65367b299685e2aa5ca93a7a1
ismail-dinar <sm41l.4796@gmail.com>
2022-05-21 12:11:46 +0100

    1 binaries uploaded

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { Table } from '../interfaces/table.interface';
 import { StateService } from '../services/state.service';
 
@@ -9,7 +10,11 @@ import { StateService } from '../services/state.service';
   styleUrls: ['./grid.component.css'],
 })
 export class GridComponent {
-  public currentTable$: Observable<Table> = this.stateService.currentTable$;
+  public message: string;
+
+  public currentTable$: Observable<Table> = this.stateService.currentTable$.pipe(tap((table) => {
+      this.message = !table ? 'Please select a table' : null;
+  }));
 
   public constructor(private stateService: StateService) {}
 }
